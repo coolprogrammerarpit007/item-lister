365dfbc074ead1823e8b5832af020718e24e12c1
arpit mishra <arpit.mishra.out@gmail.com>
2024-07-05 06:39:08 +0530

    added the delete item functuanility

--- src/index.js
+++ src/index.js
@@ -13,13 +13,33 @@ const addBtn = document.getElementById(`add`);
 // Adding event listener to add items in the cart
 
 addBtn.addEventListener(`click`, (e) => {
-  html += `
-        <div class= "flex justify-between items-center" data-item-name = item-${count}>
+  html = ``;
+  html = `
+        <div class= "flex justify-between items-center" id = item-${count}>
           <li class="uppercase text-green-600">${inputEl.value}</li> 
-          <button class="bg-red-500 px-[2rem] py-[1rem] rounded-lg" data-item-name = item-${count}>Delete</button>
+          <button class="bg-red-500 px-[2rem] py-[1rem] rounded-lg del-btn" data-item-name = item-${count}>Delete</button>
         </div>
     `;
 
-  shoppingContainer.innerHTML = html;
+  shoppingContainer.innerHTML += html;
   count++;
+
+
+  // Acessing the delete btn
+
+  const delBtns = document.querySelectorAll(`.del-btn`);
+  
+  delBtns.forEach((btn)=>{
+    btn.addEventListener(`click`,function(e){
+      let id = btn.dataset.itemName;
+      console.log(id);
+      let el = document.querySelector(`#${id}`);
+      if(el){
+        el.remove();
+      }
+      else{
+        console.log(`${el} does not exist!`);
+      }
+    })
+  })
 });
